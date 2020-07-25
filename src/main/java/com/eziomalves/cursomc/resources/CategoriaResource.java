d16561a0b1f1a30ba9635f3d068bb5820b292b5e
Eziom Alves <eziomalves@hotmail.com>
2020-07-24 21:34:36 -0300

    testando o Rest

---
+++ src/main/java/com/eziomalves/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.eziomalves.cursomc.resources;
+
+import org.springframework.beans.factory.config.YamlProcessor.ResolutionMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+	}
+}
